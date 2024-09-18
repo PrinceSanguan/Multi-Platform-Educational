518ea392cf94e3c9fde927cb463053db58b65853
bryandetera07 <bryandetera07@gmail.com>
2024-09-18 08:26:23 -0700

    commit

--- app/Filament/Admin/Resources/ModuleResource.php
+++ app/Filament/Admin/Resources/ModuleResource.php
@@ -11,6 +11,7 @@ use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -35,10 +36,11 @@ class ModuleResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('name'),
+                TextColumn::make('archive'),
             ])
             ->filters([
-                //
+                
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
