ddccebf46293c75a932f4d6fc5a0976c119a2619
blindmelon07 <bryandetera07@gmail.com>
2024-10-27 22:07:48 +0800

    added archive

---
+++ app/Filament/Admin/Resources/ArchivedModuleResource/Pages/CreateArchivedModule.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Admin\Resources\ArchivedModuleResource\Pages;
+
+use App\Filament\Admin\Resources\ArchivedModuleResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateArchivedModule extends CreateRecord
+{
+    protected static string $resource = ArchivedModuleResource::class;
+}
